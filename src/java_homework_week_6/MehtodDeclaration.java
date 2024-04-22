f3bf30eb50683c23c7ec406870c85c091044fd28
shubloha <167621900+shubloha@users.noreply.github.com>
2024-04-22 20:38:06 +0000

    Add files via upload

---
+++ src/java_homework_week_6/MehtodDeclaration.java
@@ -0,0 +1,90 @@
+package methodtypes;
+
+public class MehtodDeclaration {
+    public static void main(String[] args) {
+        System.out.println("Here are 20 methods");
+    }
+
+    public static void m1(int c, int d) {
+
+    }
+
+    public static void m2(byte g, short h) {
+
+    }
+
+    public static void m3(int a, int b) {
+
+    }
+
+    public static void m4(float e, String f) {
+
+    }
+
+    public static void m5(long h, double i) {
+
+    }
+
+    public static void m6() {
+
+    }
+
+    public static void m7() {
+
+    }
+
+    public static void m8() {
+
+    }
+
+    public static void m9() {
+
+    }
+
+    public static void m10() {
+
+    }
+
+    public void m11() {
+
+    }
+
+    public void m12() {
+
+    }
+
+    public void m13() {
+
+    }
+
+    public void m14(long j, short k) {
+
+    }
+
+    public void m15(int l, String m) {
+
+    }
+
+    public void m16(boolean n, int o) {
+
+    }
+
+    public void m17(char p, int q) {
+
+    }
+
+    public void m18(double r, float s) {
+
+    }
+
+    public void m19() {
+
+    }
+
+    public void m20() {
+
+    }
+}
+
+ // 20 method
+//10 with parameter 10 without parameter
